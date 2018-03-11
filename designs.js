68f0a3652c33eb11fff60cb01b685ae1eef05a95
Natalie Cardot <33387780+nataliecardot@users.noreply.github.com>
2018-03-11 16:25:59 -0700

    Enhance coloring features

--- designs.js
+++ designs.js
@@ -15,16 +15,24 @@ $('.size-picker').submit(function makeGrid(e) {
   // Outer for loop adds desired number of rows (grid height)
   for (let i = 1; i <= heightInput; i++) {
     $('table').append('<tr></tr>');
-    /* Inner loop adds desired number of columns as cells (td) within rows (tr) and creates a class called 'Cell' for each cell (td). Class is used later, allowing user to color cells on click. (Capitalized class 'Cell' because the Udacity Frontend Nanodegree Style Guide (JavaScript page) advises using Pascal Case for class names) */
+    // Inner loop adds desired number of columns as cells (td) within rows (tr) and creates a class called 'Cell' for each cell (td). Class is used later, allowing user to color cells on click. (Capitalized class 'Cell' because the Udacity Frontend Nanodegree Style Guide (JavaScript page) advises using Pascal Case for class names)
     for (let j = 1; j <= widthInput; j++) {
-      // ':last' is a jQuery extension (not part of CSS specification) that selects a single element by filtering the current jQuery collection and matching the last element within it. For best performance using ':last', first select element(s) using pure CSS selector, then use .filter(":last").
+      // ':last' is a jQuery extension (not part of CSS specification) that selects a single element by filtering the current jQuery collection and matching the last element within it. For best performance using ':last', first select element(s) using pure CSS selector, then use .filter(":last")
       $('tr').filter(':last').append('<td></td>');
+      // In this case .attr() method sets attribute (class) to name provided as second argument for matched elements (td)
+      $('td').attr('class', 'Cell');
     }
   }
+  // Fills in cell with chosen color when mouse button is pressed down over it. Unlike function dragColor(), doesn't require mouse to enter a cell while mouse button is being held down. Note: 'mousedown' event is fired when the mouse button is pressed but before it's released, whereas click event is fired after mousedown (click) and mouseup (release) events have completed
+  $('.Cell').mousedown(function() {
+  // Adds chosen color to cell upon a click event. Selector 'this' refers to cell (with class 'Cell') being clicked. Variable 'color' is defined here rather than globally so JS checks whether a new color has been picked before each mousedown event
+    let color = $('.color-picker').val();
+    $(this).css('background-color', color);
+  });
   dragColor();
 });
 
-// function to allow mouse-drag coloring
+// Enables mouse-drag coloring. Fills in cell when mouse pointer enters it and mouse is pressed down
 function dragColor() {
   // Filters clicks by those in cells. Note: 'mousedown' event is fired when the mouse button is pressed but before it's released, whereas 'click' event is fired after mousedown (click) and mouseup (release) events have completed
   pixelCanvas.on('mousedown', 'td', function() {
@@ -36,8 +44,8 @@ function dragColor() {
     mousedown = false;
   });
 
-  // Fills in cells with color if mouse pointer is moving and being held down over cell
-  pixelCanvas.on('mousemove', 'td', function() {
+  // 'mouseover' triggered when mouse pointer enters an element
+  pixelCanvas.on('mouseover', 'td', function() {
     // Variable 'color' is defined here rather than globally so JS checks whether a new color has been picked before each mousedown event
     let color = $('.color-picker').val();
     if (mousedown) {
