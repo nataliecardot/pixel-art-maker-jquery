3c3c2c6591fe18414cc349a893508d298cfdb7ec
Natalie Cardot <33387780+nataliecardot@users.noreply.github.com>
2018-03-26 21:37:19 -0700

    Update erase and draw modes, styling

--- designs.js
+++ designs.js
@@ -2,12 +2,18 @@
 
 const pixelCanvas = $('.pixel-canvas');
 
+let color = $('.color-picker').val();
+
+$('.color-picker').change(function() {
+    color = $('.color-picker').val();
+});
+
 // Creates default-sized grid immediately on page load
 $(window).on('load', makeGrid);
 
 // Creates grid
 function makeGrid(e) {
-  // preventDefault() method intercepts 'submit' event, which would normally submit the form and cause the page to refresh, preventing makeGrid() function from being processed
+  // preventDefault() method intercepts 'submit' event, which would normally submit form and cause page to refresh, preventing makeGrid() function from being processed
   if (e) {
     e.preventDefault();
   }
@@ -20,18 +26,17 @@ function makeGrid(e) {
   // Outer for loop adds desired number of rows (grid height)
   for (let i = 1; i <= heightInput; i++) {
     $('table').append('<tr></tr>');
-    // Inner loop adds desired number of columns as cells (td) within rows (tr) and creates a class called 'Cell' for each cell (td). Class is used later, allowing user to color cells on click. (Capitalized class 'Cell' because the Udacity Frontend Nanodegree Style Guide (JavaScript page) advises using Pascal Case for class names)
+    // Inner loop adds desired number of columns as cells (td) within rows (tr) and creates a class called 'Cell' for each cell (td). Class is used later, allowing user to color cells on click. (Capitalized class 'Cell' because Udacity Frontend Nanodegree Style Guide (JavaScript page) advises using Pascal Case for class names)
     for (let j = 1; j <= widthInput; j++) {
-      // ':last' is a jQuery extension (not part of CSS specification) that selects a single element by filtering the current jQuery collection and matching the last element within it. For best performance using ':last', first select element(s) using pure CSS selector, then use .filter(":last")
+      // ':last' is a jQuery extension (not part of CSS specification) that selects a single element by filtering current jQuery collection and matching last element within it. For best performance using ':last', first select element(s) using pure CSS selector, then use .filter(":last")
       $('tr').filter(':last').append('<td></td>');
       // here, .attr() method sets attribute (class) to name provided as second argument for matched elements (td)
       $('td').attr('class', 'Cell');
     }
   }
-  // Fills in cell with chosen color when mouse button is pressed down over it. Unlike function dragColor(), doesn't require mouse to enter a cell while mouse button is being held down. Note: 'mousedown' event is fired when the mouse button is pressed but before it's released, whereas click event is fired after mousedown (click) and mouseup (release) events have completed
+  // Fills in cell with chosen color when mouse button is pressed down over it. Unlike function dragColor(), doesn't require mouse to enter a cell while mouse button is being held down. Note: 'mousedown' event is fired when mouse button is pressed but before it's released, whereas click event is fired after mousedown (click) and mouseup (release) events have completed
   $('.Cell').mousedown(function() {
-  // Adds chosen color to cell upon a click event. Selector 'this' refers to cell (with class 'Cell') being clicked. Variable 'color' is defined here rather than globally so JS checks whether a new color has been picked before each mousedown event
-    const color = $('.color-picker').val();
+  // Adds chosen color to cell upon click event. Selector 'this' refers to cell (with class 'Cell') being clicked
     $(this).css('background-color', color);
   });
   dragColor();
@@ -41,22 +46,19 @@ $('.size-picker').submit(makeGrid);
 
 // Enables mouse-drag coloring (default mode). Fills in cell when mouse pointer enters it and mouse is pressed down
 function dragColor() {
-  // Filters clicks by those in cells. Note: 'mousedown' event is fired when the mouse button is pressed but before it's released, whereas 'click' event is fired after mousedown (click) and mouseup (release) events have completed
+  // Filters clicks by those in cells
   $(pixelCanvas).on('mousedown', 'td', function() {
     mousedown = true;
   });
 
-  // 'mouseup': when pointer is over element and mouse button has been clicked then released
+  // 'mouseup': when pointer is over element and mouse button has been clicked then released (unlike click event, doesn't have to be on same element on which mousedown occurred)
   $(document).mouseup(function() {
     mousedown = false;
   });
 
   // 'mouseover' triggered when mouse pointer enters an element
   $(pixelCanvas).on('mouseover', 'td', function() {
-    // Variable 'color' is defined here rather than globally so JS checks whether a new color has been picked before each mousedown event
-    let color = $('.color-picker').val();
     if (mousedown) {
-      // Selector 'this' refers to pixelCanvas
       $(this).css('background-color', color);
     }
   });
@@ -68,35 +70,31 @@ $(pixelCanvas).on('dblclick', 'td', function() {
 });
 // Adds color-fill functionality
 $('.quick-fill').click(function() {
-  const color = $('.color-picker').val();
   pixelCanvas.children().find('td').css('background-color', color);
 });
 
-// DRAW AND ERASE MODES:
+// (NONDEFAULT) DRAW AND ERASE MODES:
 
 // Allows for drag erasing upon clicking 'erase' button
 $('.erase-mode').click(function() {
   // Removes event handler (mousedown) enabling single-cell coloring while in default mode (inside makeGrid()). Without this, after clicking 'erase' mode upon page load, mousedown fills cell with color (but re-removes color on mouseup)
   $('.Cell').off();
   function dragErase() {
-    // Filters clicks by those in cells. Note: 'mousedown' event is fired when the mouse button is pressed but before it's released, whereas 'click' event is fired after mousedown (click) and mouseup (release) events have completed
+    // Filters clicks by those in cells
     $(pixelCanvas).on('mousedown', 'td', function() {
       mousedown = true;
     });
-    // 'mouseup': when pointer is over element and mouse button has been clicked then released
     $(document).mouseup(function() {
       mousedown = false;
     });
-    // 'mouseover' triggered when mouse pointer enters an element
     $(pixelCanvas).on('mouseover', 'td', function() {
       if (mousedown) {
-        // Selector 'this' refers to pixelCanvas
         $(this).css('background-color', '');
       }
     });
   }
   // Enables single-cell erase while in erase mode
-  $(pixelCanvas).on('click', 'td', function() {
+  $(pixelCanvas).on('mousedown', 'td', function() {
     $(this).removeAttr('style');
   });
   dragErase();
@@ -107,28 +105,20 @@ $('.draw-mode').click(function() {
   // Removes event handlers that were attached to the grid with .on(). Without this, code for single-cell erase while in erase mode prevents single-cell coloring while in draw mode
   $(pixelCanvas).off();
   function dragDrawMode() {
-    // Filters clicks by those in cells. Note: 'mousedown' event is fired when the mouse button is pressed but before it's released, whereas 'click' event is fired after mousedown (click) and mouseup (release) events have completed
     $(pixelCanvas).on('mousedown', 'td', function() {
       mousedown = true;
     });
-    // 'mouseup': when pointer is over element and mouse button has been clicked then released
     $(document).mouseup(function() {
       mousedown = false;
     });
-    // 'mouseover' triggered when mouse pointer enters an element
     $(pixelCanvas).on('mouseover', 'td', function() {
-      // Variable 'color' is defined here rather than globally so JS checks whether a new color has been picked before each mousedown event
-      let color = $('.color-picker').val();
       if (mousedown) {
-        // Selector 'this' refers to pixelCanvas
         $(this).css('background-color', color);
       }
     });
   }
   // Enables single-cell coloring while in draw mode
-  $('td').click(function() {
-  // Adds chosen color to cell upon a click event. Selector 'this' refers to cell (with class 'Cell') being clicked. Variable 'color' is defined here rather than globally so JS checks whether a new color has been picked before each mousedown event
-    const color = $('.color-picker').val();
+  $('td').mousedown(function() {
     $(this).css('background-color', color);
   });
   dragDrawMode();
